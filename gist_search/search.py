41cf4359310aae54ebb4c650ee6fef4d68f41cc0
Jordan Winfery <jwinf843@gmail.com>
2019-02-26 03:49:34 +0000

    Fixed search.py file, passing all tests for b31

--- gist_search/search.py
+++ gist_search/search.py
@@ -1,7 +1,27 @@
 from gist_search.utils import get_gists
-
+# from utils import get_gists
 
 def search_gists(username, description=None, file_name=None):
     if not description and not file_name:
         print("At least one search parameter must be specified")
         return
+    
+    gists = get_gists(username)
+    results = []
+    
+    for gist in gists:
+        if description and description not in gist['description'].lower():
+            continue
+        
+        if file_name:
+            matched = False
+            for fname in gist['files']:
+                if file_name in fname:
+                    matched = True
+                    break
+            if matched == False:
+                continue            
+        
+        results.append(gist)        
+    
+    return results
--- gist_search/utils.py
+++ gist_search/utils.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 
 BASE_URL = 'https://api.github.com/users/{username}/gists'
 
@@ -9,3 +10,11 @@ def get_gists(username):
     if not resp.ok:
         return None
     return resp.json()
+
+if __name__ == '__main__':
+    gists = get_gists('santiagobasulto')
+    first_three = gists[:3]
+
+    for gist in first_three:
+        print('{:40} | {}'.format(gist['id'], gist['description']))
+#         pprint(gist)
